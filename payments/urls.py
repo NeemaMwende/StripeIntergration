5dfc0bd572f51e9306b72801f8c6d8ff6a610571
Neema <neemamwende009@gmail.com>
2024-10-01 17:40:10 +0300

    updates

--- payments/urls.py
+++ payments/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.HomePageView.as_view(), name='home'),
-    path('pay/', views.pay, name='pay'),
-    path('success/', views.success, name='success'),
-    path('cancel/', views.cancel, name='cancel'),
+    # path('pay/', views.pay, name='pay'),
+    # path('success/', views.success, name='success'),
+    # path('cancel/', views.cancel, name='cancel'),
 ]
